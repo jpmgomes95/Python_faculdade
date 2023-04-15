8bd10122278239adaed34ee3d8f49f027c73a437
João Pedro Moura Gomes <jpmgomes95@gmail.com>
2023-04-15 11:35:05 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,12 @@
-cont = 0
-while True:
-    cont +=1
-    if (cont%2== 0):
-        continue
-    if(cont > 9):
-        break
-    print(cont,end= ";")
+lista = [4, 3, 1, 9, 8, 7, 2, 5]
+
+for passo in range(len(lista)-1,0,-1):
+
+  for i in range(passo):
+    if lista[i]>lista[i+1]:
+
+     lista[i],lista[i+1]=lista[i+1],lista[i] 
+
+     print(lista)
+
+print(18/4, 18//4, 18%4)
